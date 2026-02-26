b57e9b99789608c1175d71cc96cfded5c5dd9eaf
Clifton Warner <clifton.warner@nhcs.net>
2026-02-26 17:13:02 +0000

    File changes

--- src/pages/BusProfile.jsx
+++ src/pages/BusProfile.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import WinWindow from '../components/WinWindow';
 import AddHistoryForm from '../components/fleet/AddHistoryForm';
 import moment from 'moment';
