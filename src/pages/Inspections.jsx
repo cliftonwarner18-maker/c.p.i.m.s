55757d41d256db33046c7d0c89b5a24e3e1ff26e
Clifton Warner <clifton.warner@nhcs.net>
2026-02-27 04:20:00 +0000

    File changes

--- src/pages/Inspections.jsx
+++ src/pages/Inspections.jsx
@@ -141,7 +141,6 @@ export default function Inspections() {
             TOTAL INSPECTIONS: {filtered.length}
             </div>
             </WinWindow>
-        </div>
         </>
         );
         }
