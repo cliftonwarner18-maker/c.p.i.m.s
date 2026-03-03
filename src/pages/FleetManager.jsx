0432f8f611257ea23617370cdf30fdb7633b1560
Clifton Warner <clifton.warner@nhcs.net>
2026-03-03 15:14:15 +0000

    File changes

--- src/pages/FleetManager.jsx
+++ src/pages/FleetManager.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { jsPDF } from 'jspdf';
 import LoadingScreen from '../components/LoadingScreen';
 import BusForm from '../components/fleet/BusForm';
 import FleetTable from '../components/fleet/FleetTable.jsx';
