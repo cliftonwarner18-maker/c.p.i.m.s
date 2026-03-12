bfc2376e5ac83377ea8cc077b83c0683b0ef06b9
Clifton Warner <clifton.warner@nhcs.net>
2026-03-12 04:49:21 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { format } from 'date-fns';
 import {
